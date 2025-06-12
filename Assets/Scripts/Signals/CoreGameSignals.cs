186808aae9acfc123943595ba063143db68f9c86
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-12 15:28:52 +0300

    Core Game Signals Added

---
+++ Assets/Scripts/Signals/CoreGameSignals.cs
@@ -0,0 +1,13 @@
+﻿using Extentions;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class CoreGameSignals : MonoSingleton<CoreGameSignals>
+    {
+        public UnityAction onPlay = delegate { };
+        public UnityAction onReset = delegate { };
+        public UnityAction<Transform> onSetPlayerPosition = delegate { };
+    }
+}
