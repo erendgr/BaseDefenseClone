ad140334bfb728b3207d24acfb818397d55c48c5
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-14 17:39:09 +0300

    Player Commands Added

---
+++ Assets/Scripts/Command/Player/SetIdleInputValuesCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Controllers.Player;
+using Enums;
+using Keys;
+using NUnit.Framework.Constraints;
+using Signals;
+
+namespace Command.Player
+{
+    public class SetIdleInputValuesCommand
+    {
+        #region Self Variables
+
+        #region Private Variables
+
+        private PlayerMovementController _movementController;
+        private PlayerAnimationController _animationController;
+
+        #endregion
+
+        #endregion
+
+        public SetIdleInputValuesCommand(ref PlayerMovementController movementController,
+            ref PlayerAnimationController animationController)
+        {
+            _movementController = movementController;
+            _animationController = animationController;
+        }
+
+        public void Execute(IdleInputParams inputParams, PlayerStates playerState)
+        {
+            switch (playerState)
+            {
+                case PlayerStates.Inside:
+                    _movementController.UpdateInputValue(inputParams);
+                    _animationController.SetSpeedVariable(inputParams);
+                    break;
+                case PlayerStates.Outside:
+                    _movementController.UpdateInputValue(inputParams);
+                    _animationController.SetSpeedVariable(inputParams);
+                    _animationController.SetOutSideAnimState(inputParams, default, false);
+                    break;
+                case PlayerStates.Turret:
+                    _movementController.UpdateTurretInputValue(inputParams);
+                    break;
+                case PlayerStates.Attack:
+                    var target = AttackSignals.Instance.onGetPlayerTarget();
+                    _movementController.UpdateInputValue(inputParams);
+                    _animationController.SetSpeedVariable(inputParams);
+                    _animationController.SetOutSideAnimState(inputParams, target.transform, true);
+                    break;
+                case PlayerStates.Death:
+                    return;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Command/Player/SetPlayerStateCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Controllers.Player;
+using Enums;
+using Managers;
+
+namespace Command.Player
+{
+    public class SetPlayerStateCommand
+    {
+        #region Self Variables
+
+        #region Private Variables
+
+        private PlayerMovementController _movementController;
+        private PlayerAnimationController _animationController;
+        private PlayerHealthController _healthController;
+        private PlayerManager _manager;
+
+        #endregion
+
+        #endregion
+
+        public SetPlayerStateCommand(ref PlayerManager manager, ref PlayerMovementController movementController,
+            ref PlayerAnimationController animationController/*, ref PlayerHealthController healthController*/)
+        {
+            _manager = manager;
+            _movementController = movementController;
+            _animationController = animationController;
+            //_healthController = healthController;
+        }
+
+        public void Execute(PlayerStates playerState, PlayerAnimState weaponAnimState)
+        {
+            switch (playerState)
+            {
+                case PlayerStates.Inside:
+                    _animationController.SetBoolAnimState(PlayerAnimState.BaseState, true);
+                    _animationController.SetBoolAnimState(weaponAnimState, false);
+                    //_healthController.PlayerInBase(true);
+                    break;
+                case PlayerStates.Outside:
+                    _animationController.SetBoolAnimState(PlayerAnimState.BaseState, false);
+                    _animationController.SetBoolAnimState(weaponAnimState, true);
+                    //_healthController.PlayerInBase(false);
+                    break;
+                case PlayerStates.Attack:
+                    break;
+                case PlayerStates.Turret:
+                    break;
+                case PlayerStates.Death:
+                    _movementController.IsReadyToPlay(false);
+                    _animationController.SetBoolAnimState(PlayerAnimState.BaseState, true);
+                    _animationController.SetBoolAnimState(weaponAnimState, false);
+                    //_manager.PlayerDeath();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+
+            }    
+        }
+    }
+}
