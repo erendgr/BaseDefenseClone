236526c53d78e85d16b5bfe9539d7912ff6cd6a1
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-16 20:19:38 +0300

    Base System Added

---
+++ Assets/Scripts/Managers/BaseManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using Datas.ValueObjects;
+using Enums;
+using Keys;
+using Signals;
+using UnityEngine;
+
+namespace Managers
+{
+    public class BaseManager : MonoBehaviour
+    {
+        #region Self Variables
+        
+        #region Private Variables
+
+        private AreaDataParams _areaData;
+        private Dictionary<RoomNameEnum, int> _payedRoomDatas;
+        private Dictionary<TurretNameEnum, int> _payedTurretDatas;
+        public BaseData _data;
+
+        #endregion
+
+        #endregion
+
+        #region Event Subscription
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+
+            GetReferences();
+        }
+
+        private void SubscribeEvents()
+        {
+            BaseSignals.Instance.onBaseAreaBuyedItem += OnSetPayedRoomData;
+            BaseSignals.Instance.onTurretAreaBuyedItem += OnSetPayedTurretData;
+            // BaseSignals.Instance.onTurretData += OnGetTurretData;
+            // BaseSignals.Instance.onPayedTurretData += OnGetTurretPayedAmount;
+            SaveSignals.Instance.onGetSavedAreaData += OnGetAreaDatas;
+        }
+
+        private void UnsubscribeEvents()
+        {
+            BaseSignals.Instance.onBaseAreaBuyedItem -= OnSetPayedRoomData;
+            BaseSignals.Instance.onTurretAreaBuyedItem -= OnSetPayedTurretData;
+            // BaseSignals.Instance.onTurretData -= OnGetTurretData;
+            // BaseSignals.Instance.onPayedTurretData -= OnGetTurretPayedAmount;
+            SaveSignals.Instance.onGetSavedAreaData -= OnGetAreaDatas;
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+
+        #endregion
+
+        private void GetReferences()
+        {
+            _payedRoomDatas = SaveSignals.Instance.onLoadAreaData().RoomPayedAmount;
+            _payedTurretDatas = SaveSignals.Instance.onLoadAreaData().RoomTurretPayedAmount;
+            //SetBaseLevelText();
+        }
+        
+        private void OnSetPayedRoomData(RoomNameEnum room, int payedAmount)
+        {
+            _payedRoomDatas[room] = payedAmount;
+            AreaDataSave();
+        }
+
+        private void OnSetPayedTurretData(TurretNameEnum turret, int payedAmount)
+        {
+            _payedTurretDatas[turret] = payedAmount;
+            AreaDataSave();
+        }
+
+        private void AreaDataSave()
+        {
+            _areaData = new AreaDataParams
+            {
+                RoomPayedAmount = _payedRoomDatas,
+                RoomTurretPayedAmount = _payedTurretDatas
+            };
+            SaveSignals.Instance.onAreaDataSave?.Invoke();
+            SaveSignals.Instance.onScoreSave?.Invoke();
+        }
+
+        private AreaDataParams OnGetAreaDatas() => _areaData;
+    }
+}
---
+++ Assets/Scripts/Signals/BaseSignals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Datas.ValueObjects;
+using Enums;
+using Extentions;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class BaseSignals : MonoSingleton<BaseSignals>
+    {
+        #region Base Area
+
+        public UnityAction<RoomNameEnum,int> onBaseAreaBuyedItem = delegate {  };
+        public UnityAction<TurretNameEnum,int> onTurretAreaBuyedItem = delegate {  };
+        public UnityAction onGettedBaseData = delegate {  };
+        public Func<RoomNameEnum,RoomData> onRoomData = delegate{return  default;};
+        public Func<RoomNameEnum,int> onPayedRoomData = delegate{return  default;};
+        public Func<TurretNameEnum,BuyableTurretData> onTurretData = delegate{return  default;};
+        public Func<TurretNameEnum,int> onPayedTurretData = delegate{return  default;};
+
+        #endregion
+        
+    }
+}
