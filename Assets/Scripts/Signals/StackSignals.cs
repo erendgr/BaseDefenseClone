df1eded6d31ba6ca59c4e825f03f9c6e07551ca9
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-19 23:00:52 +0300

    Base Stack Signals Added

---
+++ Assets/Scripts/Signals/StackSignals.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Extentions;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class StackSignals : MonoSingleton<StackSignals>
+    {
+        public UnityAction<GameObject, List<GameObject>> onInteractStackHolder = delegate { };
+        public UnityAction<GameObject> onDecreseStackHolder = delegate { };
+    }
+}
