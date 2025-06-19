02ae64a7e4251fbd3e8fb01aad70db4f79cc9584
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-19 23:01:03 +0300

    Attack Signals Added

---
+++ Assets/Scripts/Signals/AttackSignals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Extentions;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class AttackSignals : MonoSingleton<AttackSignals>
+    {
+        public UnityAction<int> onDamageToPlayer = delegate { };
+        public UnityAction<bool> onPlayerHasTarget = delegate { };
+        public Func<GameObject> onGetPlayerTarget = delegate { return default; };
+    }
+}
