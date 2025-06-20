6bc8fd2528134555d472ffce095f9e8b36242bb9
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-20 14:47:34 +0300

    Ammo Physics Controller Added

---
+++ Assets/Scripts/Controllers/AmmoPhysicsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using _ObjectPooling.Scripts.Enums;
+using _ObjectPooling.Scripts.Signals;
+using Signals;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class AmmoPhysicsController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private Rigidbody rb;
+
+        #endregion
+
+        #region Private Variables
+
+        private readonly string _enemy = "Enemy";
+        private readonly string _turretAttackRadius = "TurretAttackRadius";
+        private readonly string _player = "Player"; // for test
+
+        #endregion
+
+        #endregion
+
+        private void Awake()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnDisable()
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+
+        public void SetAddForce(Vector3 direct)
+        {
+            rb.AddForce(direct, ForceMode.VelocityChange);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(_player))
+            {
+                AttackSignals.Instance.onDamageToPlayer?.Invoke(10); // for test 10 damage
+                PoolSignals.Instance.onEnqueuePooledGameObject?.Invoke(gameObject, PoolType.Ammo);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(_turretAttackRadius))
+            {
+                PoolSignals.Instance.onEnqueuePooledGameObject?.Invoke(gameObject, PoolType.Ammo);
+            }
+        }
+    }
+}
