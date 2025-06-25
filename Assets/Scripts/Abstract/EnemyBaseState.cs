af8930b2a0804af7347f1e19c55ce2bb9194e738
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-25 22:06:32 +0300

    Enemy State Machine Added

---
+++ Assets/Scripts/Abstract/EnemyBaseState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Abstract
+{
+    public abstract class EnemyBaseState
+    {
+        public abstract void EnterState();
+        
+        public abstract void UpdateState();
+        
+        public abstract void OnTriggerEnterState(Collider other);
+        
+        public abstract void OnTriggerExitState(Collider other);
+    }
+}
---
+++ Assets/Scripts/States/Enemy/ChaseToSoldier.cs
@@ -0,0 +1,66 @@
+﻿using Abstract;
+using AIBrain;
+using Datas.ValueObjects;
+using Enums;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace States.Enemy
+{
+    public class ChaseToSoldier : EnemyBaseState
+    {
+        #region Self Variables
+
+        #region Private Variables
+
+        private EnemyAIBrain _manager;
+        private NavMeshAgent _agent;
+        private EnemyTypeData _data;
+        private readonly string _soldier = "Soldier";
+
+        #endregion
+
+        #endregion
+
+        public ChaseToSoldier(ref EnemyAIBrain manager, ref NavMeshAgent agent, ref EnemyTypeData data)
+        {
+            _manager = manager;
+            _agent = agent;
+            _data = data;
+        }
+
+        public override void EnterState()
+        {
+            _manager.AttackToPlayerStatus(false);
+            _agent.speed = _data.ChaseSpeed;
+            _manager.AnimTriggerState(EnemyAnimState.Chase);
+            _agent.SetDestination(_manager.Target.transform.position);
+        }
+
+        public override void UpdateState()
+        {
+            _agent.destination = _manager.Target.transform.position;
+            if (_data.AttackRange > _agent.remainingDistance)
+            {
+                _manager.SwitchState(EnemyStates.AttackToSoldier);
+            }
+
+            if (_manager.HealthCheck())
+            {
+                _manager.SwitchState(EnemyStates.EnemyDeath);
+            }
+        }
+
+        public override void OnTriggerEnterState(Collider other)
+        {
+        }
+
+        public override void OnTriggerExitState(Collider other)
+        {
+            if (other.CompareTag(_soldier))
+            {
+                _manager.SwitchState(EnemyStates.MoveToTurret);
+            }
+        }
+    }
+}
