d61ea2aac48eab8ebb615ef00e41dac8c5f5b528
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-27 20:26:09 +0300

    Enemy AI System Added

---
+++ Assets/Scripts/Controllers/EnemyBodyPhysicsController.cs
@@ -0,0 +1,45 @@
+﻿using AIBrain;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class EnemyBodyPhysicsController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private EnemyAIBrain enemyBrain;
+
+        #endregion
+
+        #region Private Variables
+
+        private readonly string _bullet = "Bullet";
+        private readonly string _turretAmmo = "TurretAmmo";
+        private readonly string _soldierBullet = "SoldierBullet";
+
+        #endregion
+
+        #endregion
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(_bullet))
+            {
+                enemyBrain.TakeBulletDamage();
+                return;
+            }
+
+            if (other.CompareTag(_turretAmmo))
+            {
+                enemyBrain.TakeAmmoDamage();
+            }
+
+            if (other.CompareTag(_soldierBullet))
+            {
+                enemyBrain.TakeSoldierDamage();
+            }
+        }
+    }
+}
