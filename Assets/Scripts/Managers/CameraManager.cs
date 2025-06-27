c311ccc71f0d2c38b850f6364dbb8dd0a540c1fe
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-27 20:23:58 +0300

    Camera System Added

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Signals;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace Managers
+{
+    public class CameraManager : MonoBehaviour
+    {
+        [SerializeField] private CinemachineCamera _camera;
+
+        #region Event Subscriptions
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CoreGameSignals.Instance.onSetPlayerPosition += OnSetCameraTarget;
+        }
+
+        private void UnsubscribeEvents()
+        {
+            CoreGameSignals.Instance.onSetPlayerPosition -= OnSetCameraTarget;
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+
+        #endregion
+
+        private void OnSetCameraTarget(Transform playerTransform)
+        {
+            _camera.Follow = playerTransform;
+        }
+    }
+}
