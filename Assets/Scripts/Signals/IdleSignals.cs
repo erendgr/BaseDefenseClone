59e86244453a94943d6d19c2d3b7d4463a95de1f
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-27 20:28:59 +0300

    Idle Signals Added

---
+++ Assets/Scripts/Signals/IdleSignals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Enums;
+using Extentions;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class IdleSignals : MonoSingleton<IdleSignals>
+    {
+        public UnityAction<GameObject,EnemyType> onEnemyDead = delegate(GameObject arg0, EnemyType type) {  };
+        public Func<GameObject> onEnemyHasTarget = delegate { return default;};
+        public UnityAction<GameObject> onHostageCollected = delegate {  };
+
+        public UnityAction onInteractPlayerWithTurret = delegate { };
+        
+        public Func<WeaponType> onSelectedWeapon = delegate { return WeaponType.Pistol;};
+        public Func<PlayerAnimState> onGetSelectedWeaponAnimState = delegate { return 0;};
+        public Func<PlayerAnimState> onGetSelectedWeaponAttackAnimState = delegate { return 0;};
+
+        public UnityAction onPlayerDied = delegate {  };
+
+    }
+}
