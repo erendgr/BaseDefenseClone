17a348a43bce9e69aff16065fdf64cccf0275792
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-29 23:33:09 +0300

    Worker Buy Area System Added

---
+++ Assets/Scripts/Controllers/Worker/AmmoWorkerBuyAreaPhysicsController.cs
@@ -0,0 +1,40 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace Controllers.Worker
+{
+    public class AmmoWorkerBuyAreaPhysicsController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region SerializeField Variables
+
+        [SerializeField] private AmmoWorkerBuyAreaManager areaManager;
+
+        #endregion
+
+        #region Private Variables
+
+        private readonly string _player = "Player";
+
+        #endregion
+        
+        #endregion
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(_player))
+            {
+                areaManager.BuyAreaEnter();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(_player))
+            {
+                areaManager.BuyAreaExit();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/Worker/MoneyWorkerBuyAreaPhysicsController.cs
@@ -0,0 +1,40 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace Controllers.Worker
+{
+    public class MoneyWorkerBuyAreaPhysicsController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region SerializeField Variables
+
+        [SerializeField] private MoneyWorkerBuyAreaManager areaManager;
+
+        #endregion
+        
+        #region Private Variables
+
+        private readonly string _player = "Player";
+
+        #endregion
+
+        #endregion
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(_player))
+            {
+                areaManager.BuyAreaEnter();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(_player))
+            {
+                areaManager.BuyAreaExit();
+            }
+        }
+    }
+}
