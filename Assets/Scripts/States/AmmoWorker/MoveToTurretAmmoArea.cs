090b35a227a88d6551bdda8792138616dabbfb22
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-29 23:29:01 +0300

    Ammo Worker State Machine Added

---
+++ Assets/Scripts/States/AmmoWorker/MoveToTurretAmmoArea.cs
@@ -0,0 +1,48 @@
+﻿using Abstract;
+using AIBrain;
+using Enums;
+using Signals;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace States.AmmoWorker
+{
+    public class MoveToTurretAmmoArea : AmmoWorkerBaseState
+    {
+        #region Self Variables
+
+        #region Private Variables
+
+        private NavMeshAgent _agent;
+        private AmmoWorkerAIBrain _manager;
+        private readonly string _turretAmmoArea = "TurretAmmoArea";
+
+        #endregion
+
+        #endregion
+
+        public MoveToTurretAmmoArea(ref AmmoWorkerAIBrain manager, ref NavMeshAgent agent)
+        {
+            _manager = manager;
+            _agent = agent;
+        }
+
+        public override void EnterState()
+        {
+            _manager.Target = WorkerSignals.Instance.onGetTurretArea();
+            if (_manager.Target == null) return;
+            _agent.SetDestination(_manager.Target.transform.position);
+            _manager.AnimTriggerState(WorkerAnimState.Walk);
+        }
+
+        public override void OnTriggerEnterState(Collider other)
+        {
+            if (other.CompareTag(_turretAmmoArea))
+            {
+                if ((_agent.remainingDistance > 1.5f)) return;
+                _manager.TurretAmmoArea(other.gameObject);
+                _manager.SwitchState(AmmoWorkerStates.WaitToAmmoArea);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/States/AmmoWorker/MoveToWareHouse.cs
@@ -0,0 +1,45 @@
+﻿using Abstract;
+using AIBrain;
+using Enums;
+using Signals;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace States.AmmoWorker
+{
+    public class MoveToWareHouse : AmmoWorkerBaseState
+    {
+        #region Self Variables
+
+        #region Private Variables
+
+        private AmmoWorkerAIBrain _manager;
+        private NavMeshAgent _agent;
+        private readonly string _ammoReloadArea = "AmmoReloadArea";
+
+        #endregion
+
+        #endregion
+
+        public MoveToWareHouse(ref AmmoWorkerAIBrain manager, ref NavMeshAgent agent)
+        {
+            _manager = manager;
+            _agent = agent;
+        }
+
+        public override void EnterState()
+        {
+            _manager.Target = IdleSignals.Instance.onGetWareHousePositon().gameObject;
+            _agent.SetDestination(_manager.Target.transform.position);
+            _manager.AnimTriggerState(WorkerAnimState.Walk);
+        }
+
+        public override void OnTriggerEnterState(Collider other)
+        {
+            if (other.CompareTag(_ammoReloadArea))
+            {
+                _manager.SwitchState(AmmoWorkerStates.WaitForFullStack);
+            }
+        }
+    }
+}
