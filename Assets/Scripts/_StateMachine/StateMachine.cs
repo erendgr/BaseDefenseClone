a1b1fc0e3b575028b21cb2d30d4e1358dd117f12
erendgr <49555544+erendgr@users.noreply.github.com>
2025-07-01 22:30:53 +0300

    State Machine Added

---
+++ Assets/Scripts/_StateMachine/StateMachine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _StateMachine
+{
+    public class StateMachine<TStateEnum, TState> where TState : class
+    {
+        private readonly Dictionary<TStateEnum, TState> _states;
+        private TState _currentState;
+
+        public StateMachine(Dictionary<TStateEnum, TState> states)
+        {
+            _states = states;
+        }
+
+        public TState Switch(TStateEnum newState)
+        {
+            if (_states.TryGetValue(newState, out var nextState))
+            {
+                _currentState = nextState;
+                return nextState;
+            }
+
+            Debug.LogWarning($"State not found: {newState}");
+            return null;
+        }
+    }
+}
