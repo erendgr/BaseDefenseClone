baf86ee2c0b9341d46fbd07a96017377727a52ff
erendgr <49555544+erendgr@users.noreply.github.com>
2025-07-01 22:28:38 +0300

    Collection Extension Script Added

---
+++ Assets/Scripts/Extentions/CollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Extentions
+{
+    public static class CollectionExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this ICollection<T> list)
+        {
+            return list == null || list.Count == 0;
+        }
+    }
+}
